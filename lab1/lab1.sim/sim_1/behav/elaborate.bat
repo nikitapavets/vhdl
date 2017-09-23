b01185ab93b765ffc9ffc55a867115d534d9e702
nikitapavets <nikitapavets@gmail.com>
2017-09-23 22:55:13 +0300

    remove

--- lab1/lab1.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\BSUIR\\Xilinx\\Vivado\\2015.2\\bin
-call %xv_path%/xelab  -wto 4d8454aefa9845ad8bd230ec49311da8 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot main_tb_behav xil_defaultlib.main_tb -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
